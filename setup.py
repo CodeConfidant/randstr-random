969f9768939d7736b0a74864f769014b99901e0c
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2020-04-24 11:58:01 -0400

    Created README.txt & updated files/archive

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     py_modules=['randstr'],
     license='MIT',
     description='A Small Python module for generating strings with random characters built on top of the random module.',
-    long_description=open('README.md').read(),
+    long_description=open('README.txt').read(),
     url='https://github.com/CodeConfidant/randstr-random',
     author='Drew Hainer',
     author_email='codeconfidant@gmail.com'
